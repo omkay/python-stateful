37e7ed905bcd4f931d30fa18edd0fa7bddc38792
Hisham Zarka <hzarka@gmail.com>
2013-02-02 19:49:51 +0400

    .

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='stateful',
+    version='0.1',
+    author='Hisham Zarka',
+    author_email='hzarka@gmail.com',
+    packages = find_packages('.'),
+    package_dir = {'': '.'},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    zip_safe=True,
+)
