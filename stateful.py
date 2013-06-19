61c18598aa735a1fc4e608e9cefebed4c6605531
Hisham Zarka <hzarka@gmail.com>
2013-06-19 19:50:46 +0400

    .

--- stateful.py
+++ stateful.py
@@ -1,6 +1,7 @@
 import sqlalchemy, logging
 from time import sleep
 import inspect, sys
+from itertools import groupby
 
 def work_generator(fn):
     while True:
@@ -40,13 +41,14 @@ class GracefulInterruptHandler(object):
         return True
 
 class Stateful(object):
-    def __init__(self, engine, work_fn, table="state", logger=None, sleep=0, work_kwargs=None):
+    def __init__(self, engine, work_fn, table="state", logger=None, sleep=0, work_kwargs=None, group_fn=None):
         self.engine = sqlalchemy.create_engine(engine)
         self.table = table
         self.logger = logger or logging.getLogger('stateful')
         self.sleep = sleep
         self.work_kwargs = work_kwargs or {}
         self.work_fn = work_fn
+        self.group_fn = group_fn
         self.engine.execute("CREATE TABLE IF NOT EXISTS %s (id VARCHAR(50) PRIMARY KEY, t TIMESTAMP DEFAULT CURRENT_TIMESTAMP)" % self.table)
 
     def finish(self, id):
@@ -63,20 +65,43 @@ class Stateful(object):
         return gen
 
     def work(self, tasks_list):
-        tasks = dict(((task[0] if isinstance(task, tuple) else task), task) for task in tasks_list)
-        new_tasks = set(tasks.keys()) - self.get_tasks()
+        finished_tasks = self.get_tasks()
+        get_task_key = lambda task: (task[0] if isinstance(task, tuple) else task)
+        tasks = [task for task in tasks_list if get_task_key(task) not in finished_tasks]
+        fn = self.worker_group if callable(self.group_fn) else self.worker_nogroup
+        return fn(tasks)
 
+    def worker_nogroup(self, tasks):
         gen = self.get_work_generator()
         with GracefulInterruptHandler(signal.SIGTERM, lambda: sys.exit(143)) as ih:
-            for i, task in enumerate(new_tasks):
+            for i, task in enumerate(tasks):
                 ih.safe_interrupt()
                 try:
                     if i != 0 and self.sleep: sleep(self.sleep)
                     self.logger.info("Working on {}".format(task))
-                    gen.send(dict(task=tasks[task], **self.work_kwargs))
+                    gen.send(dict(task=task, **self.work_kwargs))
                     self.finish(task)
                 except Exception:
                     self.logger.warn("Error while processing {}".format(task), exc_info=1)
+                    gen = self.get_work_generator()
+                    ih.safe_interrupt()
+                    sleep(self.sleep*2 or 10)
+
+    def worker_group(self, tasks):
+        tasks = tasks.sort(key=self.group_fn)
+        gen = self.get_work_generator()
+        with GracefulInterruptHandler(signal.SIGTERM, lambda: sys.exit(143)) as ih:
+            for i, (key, group) in enumerate(groupby(tasks, key=self.group_fn)):
+                ih.safe_interrupt()
+                try:
+                    if i != 0 and self.sleep: sleep(self.sleep)
+                    self.logger.info("Working on {}: {}".format(key, group))
+                    group = list(group)
+                    gen.send(dict(key=key, tasks=group, **self.work_kwargs))
+                    for task in group:
+                        self.finish(task)
+                except Exception:
+                    self.logger.warn("Error while processing {}: {}".format(key, group), exc_info=1)
                     gen = self.get_work_generator()
                     ih.safe_interrupt()
                     sleep(self.sleep*2 or 10)
